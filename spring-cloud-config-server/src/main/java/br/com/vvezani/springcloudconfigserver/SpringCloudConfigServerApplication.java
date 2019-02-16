490da26c3dbf04ace5659252c7b868b1433d81ce
Vitor Rodrigo Vezani <vitor.vezani@freshcells.de>
2019-02-16 17:57:05 +0100

    Initialize repo

---
+++ spring-cloud-config-server/src/main/java/br/com/vvezani/springcloudconfigserver/SpringCloudConfigServerApplication.java
@@ -0,0 +1,14 @@
+package br.com.vvezani.springcloudconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudConfigServerApplication.class, args);
+	}
+
+}
+
